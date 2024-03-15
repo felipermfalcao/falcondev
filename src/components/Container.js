eeecefa9b4589d647f4b1fcc439a6d2e0fe5eed7
Felipe <contato@felipefalcao.com.br>
2024-03-15 16:25:44 -0300

    Mudança de projeto

--- src/components/Container.js
+++
@@ -1,9 +0,0 @@
-import { Container, Card, Row, Text, Col, Spacer } from "@nextui-org/react";
-
-export default function ContainerPrincipal({ children }){
-    return(
-        <Container gap={0} xs>
-            { children }
-        </Container>
-    );
-}
